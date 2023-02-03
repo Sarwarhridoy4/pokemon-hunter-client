2efcbef383ee84b6a3b07e916c472df01d9d635d
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2023-02-03 16:17:58 +0600

    initial commit

---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const HomePage = () => {
+    return (
+        <div className='min-h-screen' style={{ backgroundImage: `url(https://i.ibb.co/86XnRmc/Background.png)` }}>
+            <div className="texture min-h-screen" style={{ backgroundImage: `url(https://i.ibb.co/Y2BSNnw/Texture.png)` }}>
+            
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
